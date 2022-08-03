57d06619116a1b3b7b5cb640d3bbd377e4e221ed
zbob889 <zroberts889@gmail.com>
2022-08-03 14:57:26 +0200

    Done for the day

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -3,12 +3,33 @@ import React, { Component } from 'react';
 class App extends Component {
   constructor() {
     super();
+
+    this.state = {
+      identity: {
+        firstName: 'First Name',
+        lastName: 'Last Name',
+        email: 'Email',
+        phoneNumber: 'Phone Number',
+      }
+    };
   };
 
   render() {
 
     return(
-      <div></div>
+      <div id='container'>
+        <div id='form'>
+          <div id='title'>CV Generator</div>
+
+          <div id='buttonDiv'>
+            <button id='previewButton'>Preview</button>
+          </div>
+
+          
+
+
+        </div>
+      </div>
     );
   };
 };
